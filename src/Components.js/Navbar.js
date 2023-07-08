175cccc54091b69d957a086418655a5df507cf1d
Dhritishah03 <1032211764@mitwpu.in>
2023-07-09 00:13:45 +0530

    Dhriti

--- src/Components.js/Navbar.js
+++ src/Components.js/Navbar.js
@@ -9,7 +9,7 @@
      <nav>
         <a href='#' className='logo'>
         <Link to='/'>
-           <h1 className='text-2xl lg:text-[28px] font-semibold leading-none mb-6 mt-6 pl-[-25px]'>Aadya Ventures.</h1>
+           <h1 className='text-2xl text-white lg:text-[28px] font-semibold leading-none mb-6 mt-6 pl-[-25px]'>Aadya Ventures.</h1>
          </Link>
         </a>
          <input className='menu-btn' type='checkbox' id='menu-btn'></input>
--- src/Components.js/Poster.js
+++ src/Components.js/Poster.js
@@ -5,13 +5,13 @@ import c from '../images/d.avif'
 
 const Poster = () => {
   return (
-    <section className=' bg-[url(C:\RealEstateWebApp\RealEstateWebApp\src\images\d.avif)] mt-[270px] mb-[200px] ml-[-40px] mr-[-40px] h-full max-h-[640px] pt-[50px] mb-8 xl:mb-8'>
+    <section className=' bg-[url(C:\RealEstateWebApp\RealEstateWebApp\src\Components.js\prop.jpg)] bg-cover mt-[-80px] ml-[-40px] mr-[-40px] h-full min-h-[1040px] pt-[50px] mb-8 xl:mb-8'>
       <div>
         <div className='lg:ml-8 xl:ml-[135px]  items-center lg:items-start text-center lg:text-center justify-center flex-1 px-4 lg:px-0'>
-          <h1 className='ml-[-80px] text-4xl lg:text-[58px] font-semibold leading-none mt-[-190px] mb-[300px]'>
-            Welcome To      <span className='text-cyan-600 break-words'>Real Estate</span>
+          <h1 className='text-white mt-[150px] ml-[-80px] text-4xl lg:text-[58px] font-semibold leading-none '>
+            Welcome To      <span className='text-cyan-500 break-words'>Real Estate</span>
           </h1>
-          <p className='ml-[-110px] mt-[-290px] mb-[300px] text-cyan-900'>
+          <p className='ml-[-110px] mt-[10px] mb-[300px] text-white'>
            We Help You Find Your Dream House
           </p>
         </div>
