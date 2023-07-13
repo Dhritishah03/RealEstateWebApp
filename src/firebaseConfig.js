83cd79886684605f53664899ff1a12cc3d55bca1
Sana-0511 <svnkanbargi@gmail.com>
2023-07-13 22:08:45 +0530

    Merge branch 'main' of https://github.com/Dhritishah03/RealEstateWebApp

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+import 'firebase/compat/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBSuvPaa2E0BHD2JNr6MkRNG0edql55Y6s",
   authDomain: "aadyaventure.firebaseapp.com",
@@ -17,6 +13,8 @@ const firebaseConfig = {
   measurementId: "G-71Q4K6JKZ6"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+const auth = firebase.auth()
+const db = firebase.firestore();
+export { firebase, storage, db, auth };
